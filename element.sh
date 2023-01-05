4f62a0f66b4734bfe708059b502e1da8904ea375
Temitope Babatunde <88645112+IamTemitope@users.noreply.github.com>
2023-01-05 11:03:00 +0100

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
-PSQL="psql -x --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
 
 if [[ $1 ]]
   then 
     if [[ ! $1 =~ ^[0-9]+$ ]]
      then
-        ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements using(atomic_number) JOIN types using(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
+        ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements using(atomic_number) JOIN types using(type_id) WHERE name ='$1' or symbol ='$1' ORDER BY atomic_number LIMIT 1")
       else
          ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements using(atomic_number) JOIN types using(type_id) WHERE elements.atomic_number = $1")
     fi
@@ -14,11 +14,11 @@ if [[ $1 ]]
         then
           echo "I could not find that element in the database."
         else
-          echo $ELEMENT | while IFS="|" read ATOMIC_NUMBER ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS SYMBOL NAME TYPE
+          echo $ELEMENT | while IFS=\| read ATOMIC_NUMBER ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS SYMBOL NAME TYPE
           do
-            echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
-          done
+              echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+          done 
       fi
   else
-  echo "Please provide an element as an argument."
+    echo "Please provide an element as an argument."
 fi  
